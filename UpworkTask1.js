e7215d8097be7d0f0c39d5ba4c921901d5f9930c
Hawkseye <hawkseyehd@gmail.com>
2022-07-11 23:47:26 +0500

    upwork task

--- UpworkTask1.js
+++ UpworkTask1.js
@@ -1,17 +1,13 @@
 function findVovels(value) {
     var data = value.match(/[aeiou]/gi);
-    if (data === null)
-        return null;
-    else
-        return data.length;
+    return data === null ? 0 : data;
+
 }
 
 function findLongestWord(sample) {
     sentance = sample.replace(/[#_,<.>!'"`]/g, '');
     const words = sentance.split(' ');
-    const sortedWords = words.sort((a, b) => b.length - a.length);
-
-    const longestWord = sortedWords.reduce((longest, current) => {
+    const longestWord = words.reduce((longest, current) => {
         if (longest.length === current.length)
             return findVovels(longest) > findVovels(current) ? longest : current;
         else if (longest.length > current.length)
